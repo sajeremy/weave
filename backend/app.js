fb0a0731ccd91efdc1c2a009afe5d7c56ffcd924
jkhbuild <jkh.build@gmail.com>
2022-12-18 16:50:51 -0500

    complete user auth login

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,9 @@ const cors = require("cors");
 const csurf = require("csurf");
 const { isProduction } = require("./config/keys");
 require("./models/User");
+require("./config/passport");
 
+const passport = require("passport");
 const usersRouter = require("./routes/api/users");
 const csrfRouter = require("./routes/api/csrf");
 
@@ -17,6 +19,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
 
 // Security Middleware
 if (!isProduction) {
