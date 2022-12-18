15a9f31390bd0c197705d4571d78602aa3e0c547
“sajeremy” <9jsantiago4@gmail.com>
2022-12-18 15:00:19 -0500

    made upto the express error handling section

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,43 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const cors = require("cors");
+const csurf = require("csurf");
+const { isProduction } = require("./config/keys");
 
-var app = express();
+const usersRouter = require("./routes/api/users");
+const csrfRouter = require("./routes/api/csrf");
 
-app.use(logger('dev'));
+const app = express();
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Security Middleware
+if (!isProduction) {
+  // Enable CORS only in development because React will be on the React
+  // development server (http://localhost:3000). (In production, React files
+  // will be served statically on the Express server.)
+  app.use(cors());
+}
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
+// Attach Express routers
+app.use("/api/users", usersRouter);
+app.use("/api/csrf", csrfRouter);
 
 module.exports = app;
