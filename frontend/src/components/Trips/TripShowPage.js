94c1c1c55b4e139416543ec2bf0c81ee2545dcf0
bonnieli51 <bonnieli51@gmail.com>
2022-12-20 14:21:14 -0500

    tripshowpage done

--- frontend/src/components/Trips/TripShowPage.js
+++ frontend/src/components/Trips/TripShowPage.js
@@ -6,16 +6,21 @@ import { fetchTrip, clearTripErrors } from "../../store/trips";
 function TripShowPage() {
   const dispatch = useDispatch();
   const { tripId } = useParams();
-  const trip = useSelector((state) => Object.values(state.trips.trip));
-
+  const trip = useSelector((state) =>
+    state.trips.trip ? state.trips.trip : {}
+  );
+  // console.log(trip);
   useEffect(() => {
     dispatch(fetchTrip(tripId));
     return () => dispatch(clearTripErrors());
-  }, [dispatch]);
+  }, [dispatch, tripId]);
 
   return (
     <>
       <h1>{trip.name}</h1>
+      <div>{trip.description}</div>
+      <div>{trip.startDate}</div>
+      <div>{trip.endDate}</div>
     </>
   );
 }
