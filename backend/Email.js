942b9ac94dd78dc80c75260a249947602407ebd2
“sajeremy” <9jsantiago4@gmail.com>
2022-12-21 19:48:23 -0500

    adding seeds for trips

--- backend/Email.js
+++ backend/Email.js
@@ -18,7 +18,7 @@ module.exports = function (tripDetails) {
     from: username,
     to: tripDetails.invitedUserEmail,
     subject: `Weaver Trip Invitation: ${tripDetails.ownerName}'s ${tripDetails.tripName} `,
-    text: `Hi ${tripDetails.invitedUserName},\n ${tripDetails.ownerName} will like to invite you to their upcoming trip: ${tripDetails.tripName}.  If you wish to participate please accept your invitation below: \n https://weave.onrender.com/trips/${tripDetails.tripdId}/users/${tripDetails.invitedUserId}/add`,
+    text: `Hi ${tripDetails.invitedUserName},\n ${tripDetails.ownerName} will like to invite you to their upcoming trip: ${tripDetails.tripName}.  If you wish to participate please accept your invitation below: \n https://weave.onrender.com/trips/${tripDetails.tripdId}/invite`,
   };
 
   mailTransporter.sendMail(details, (err) => {
--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -3,6 +3,10 @@ const { mongoURI: db } = require("../config/keys.js");
 const User = require("../models/User");
 const Trip = require("../models/Trip");
 const bcrypt = require("bcryptjs");
+const { faker } = require("@faker-js/faker");
+
+const NUM_SEED_USERS = 10;
+const NUM_SEED_TRIPS = 5;
 
 // Create users
 const users = [];
@@ -10,14 +14,41 @@ const users = [];
 // Create users
 users.push(
   new User({
-    firstName: "demo",
-    lastName: "user",
+    firstName: "Demo",
+    lastName: "User",
     email: "demo@user.io",
     hashedPassword: bcrypt.hashSync("password", 10),
   })
 );
 
+for (let i = 1; i < NUM_SEED_USERS; i++) {
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  users.push(
+    new User({
+      email: faker.internet.email(firstName, lastName),
+      hashedPassword: bcrypt.hashSync(faker.internet.password(), 10),
+    })
+  );
+}
+
 // Create trips
+const trips = [];
+
+for (let i = 1; i < NUM_SEED_TRIPS; i++) {
+  trips.push(
+    new Trip({
+      owner: users[Math.floor(Math.random() * NUM_SEED_USERS)]._id,
+      members: [users[1], users[2], users[3], users[4]],
+      startDate: "2023-01-17",
+      endDate: "2023-01-24",
+      name: "Vacation to Puerto Rico",
+      locations: [
+        { title: "app academy", coordinates: { lat: -40.01, lon: 32 } },
+      ],
+    })
+  );
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
