c9c03f8a3a754cd15a2dfe4de7e880c140493420
bonnieli51 <bonnieli51@gmail.com>
2022-12-22 10:20:57 -0500

    delete place item

--- frontend/src/components/Places/PlaceIndex.js
+++ frontend/src/components/Places/PlaceIndex.js
@@ -1,14 +1,28 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import PlaceIndexItem from "./PlaceIndexItem";
 
-function PlaceIndex() {
-  const places = useSelector((state) => state.trips.trip.locations);
+function PlaceIndex({ trip }) {
+  // const places = useSelector((state) =>
+  //   state.trips.trip.locations ? state.trips.trip.locations : []
+  // );
+  console.log(trip.locations);
+
+  useEffect (()=>{
+    
+  },[trip.locations])
 
   return (
     <>
-      {places.map((place) => {
-        <PlaceIndexItem key={place._id} place={place} />;
-      })}
+      <div>Places to Go</div>
+
+      {trip.locations &&
+        trip.locations.map((place, index) => (
+          <>
+            <div></div>
+            <PlaceIndexItem key={place._id} place={place} index={index} />
+          </>
+        ))}
     </>
   );
 }
