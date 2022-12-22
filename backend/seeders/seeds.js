2aacc6a10bad606cc05a5e92d05122612535ab24
“sajeremy” <9jsantiago4@gmail.com>
2022-12-22 00:19:25 -0500

    add seeds

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -6,17 +6,15 @@ const bcrypt = require("bcryptjs");
 const { faker } = require("@faker-js/faker");
 
 const NUM_SEED_USERS = 10;
-const NUM_SEED_TRIPS = 5;
 
 // Create users
 const users = [];
 
-// Create users
 users.push(
   new User({
     firstName: "Demo",
     lastName: "User",
-    email: "demo@user.io",
+    email: "demouser@user.io",
     hashedPassword: bcrypt.hashSync("password", 10),
   })
 );
@@ -26,6 +24,8 @@ for (let i = 1; i < NUM_SEED_USERS; i++) {
   const lastName = faker.name.lastName();
   users.push(
     new User({
+      firstName: firstName,
+      lastName: lastName,
       email: faker.internet.email(firstName, lastName),
       hashedPassword: bcrypt.hashSync(faker.internet.password(), 10),
     })
@@ -35,20 +35,75 @@ for (let i = 1; i < NUM_SEED_USERS; i++) {
 // Create trips
 const trips = [];
 
-for (let i = 1; i < NUM_SEED_TRIPS; i++) {
-  trips.push(
-    new Trip({
-      owner: users[Math.floor(Math.random() * NUM_SEED_USERS)]._id,
-      members: [users[1], users[2], users[3], users[4]],
-      startDate: "2023-01-17",
-      endDate: "2023-01-24",
-      name: "Vacation to Puerto Rico",
-      locations: [
-        { title: "app academy", coordinates: { lat: -40.01, lon: 32 } },
-      ],
-    })
-  );
-}
+trips.push(
+  new Trip({
+    owner: users[0]._id,
+    members: [users[1], users[2], users[3], users[4]],
+    startDate: "2023-01-17",
+    endDate: "2023-01-24",
+    name: "Vacation in Puerto Rico",
+    locations: [
+      {
+        title: "Parque de Bombas",
+        coordinates: { lat: 18.012, lon: -66.614 },
+      },
+      {
+        title: "La Cueva del Indio",
+        coordinates: { lat: 18.493, lon: -66.642 },
+      },
+      {
+        title: "Puente de Piedra",
+        coordinates: { lat: 17.934, lon: -67.196 },
+      },
+    ],
+  })
+);
+trips.push(
+  new Trip({
+    owner: users[0]._id,
+    members: [users[5], users[6], users[7]],
+    startDate: "2023-02-06",
+    endDate: "2023-02-09",
+    name: "Autonomous Vehicles Conference in Detroit",
+    locations: [
+      {
+        title: "Campus Martius Park",
+        coordinates: { lat: 42.332, lon: -83.047 },
+      },
+      {
+        title: "Fowling Warehouse Hamtramck",
+        coordinates: { lat: 42.393, lon: -83.044 },
+      },
+      {
+        title: "University of Michigan Transportation Research Institute",
+        coordinates: { lat: 42.298, lon: -83.703 },
+      },
+    ],
+  })
+);
+trips.push(
+  new Trip({
+    owner: users[8]._id,
+    members: [users[0], users[2], users[3], users[7]],
+    startDate: "2023-03-02",
+    endDate: "2023-03-09",
+    name: "Atlanta Salsa Bachata Festival",
+    locations: [
+      {
+        title: "Little Five Points",
+        coordinates: { lat: 33.764, lon: -84.349 },
+      },
+      {
+        title: "Ponce City Market",
+        coordinates: { lat: 33.774, lon: -84.366 },
+      },
+      {
+        title: "Piedmont Park",
+        coordinates: { lat: 33.787, lon: -84.376 },
+      },
+    ],
+  })
+);
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -66,7 +121,9 @@ const insertSeeds = () => {
 
   User.collection
     .drop()
+    .then(() => Trip.collection.drop())
     .then(() => User.insertMany(users))
+    .then(() => Trip.insertMany(trips))
     .then(() => {
       console.log("Done!");
       mongoose.disconnect();
