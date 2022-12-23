2d9421ecd0c5825f825bdce70b851346c5327e73
bonnieli51 <bonnieli51@gmail.com>
2022-12-22 19:39:51 -0500

    invitation page css

--- frontend/src/components/Invite/InvitationPage.js
+++ frontend/src/components/Invite/InvitationPage.js
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
+import moment from "moment";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { fetchTrip, updateTrip } from "../../store/trips";
-import "./InvitationPage.css";
+import { fetchUser } from "../../store/users";
+import "./InvitationPage.scss";
 
 function InvitationPage() {
   const dispatch = useDispatch();
@@ -11,6 +13,9 @@ function InvitationPage() {
   const currentUser = useSelector((state) => state.session.user);
   const trip = useSelector((state) =>
     state.trips.trip ? state.trips.trip : {}
+  );
+  const owner = useSelector((state) =>
+    state.users.user ? state.users.user : {}
   );
 
   const handleAccept = () => {
@@ -29,12 +34,26 @@ function InvitationPage() {
   };
   useEffect(() => {
     dispatch(fetchTrip(tripId));
-  }, [dispatch, tripId]);
+    if (trip) dispatch(fetchUser(trip.owner));
+  }, [dispatch]);
 
   return (
-    <div id="buttons">
-      <button onClick={handleAccept}>Accept </button>
-      <button onClick={handleDecline}>Decline </button>
+    <div id="Invitation-Page">
+      <div>You're Invited to:</div>
+      <div>
+        {owner && owner.firstName} {owner && owner.lastName}'s Trip
+      </div>
+      <div>{trip.name}</div>
+      <div> From {moment(trip.startDate).utc().format("MM-DD-YYYY")}</div>
+      <div> To {moment(trip.endDate).utc().format("MM-DD-YYYY")}</div>
+      <div id="invitation-buttons">
+        <button id="accept" onClick={handleAccept}>
+          Accept
+        </button>
+        <button id="decline" onClick={handleDecline}>
+          Decline
+        </button>
+      </div>
     </div>
   );
 }
