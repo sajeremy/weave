e15214b80bc110ad44db96db72813eea1400492d
“sajeremy” <9jsantiago4@gmail.com>
2022-12-23 02:35:02 -0500

    update links to member invitation

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -79,7 +79,8 @@ trips.push(
           "Saturday: 9:30 AM - 5:00 PM",
           "Sunday: 9:30 AM - 5:00 PM",
         ],
-        photo: "https://maps.google.com/maps/contrib/105988571914224840772",
+        photo:
+          "https://lh3.googleusercontent.com/places/AJDFj43istbo2o2rmXZZpH-484pw7PODn_zKOHvuHam2u4TJSaYKPa-E1atLC1wKXOKjfzLO1n1tk6U0_MXvlOk-UUcGnw5CXUvTjfI=s1600-w3000",
         rating: 4.7,
         title: "Parque de Bombas",
         website: "",
@@ -96,7 +97,8 @@ trips.push(
           "Saturday: 9:30 AM - 5:00 PM",
           "Sunday: 9:30 AM - 5:00 PM",
         ],
-        photo: "https://maps.google.com/maps/contrib/110915644480179389748",
+        photo:
+          "https://lh3.googleusercontent.com/places/AJDFj42cAOWuCm5s6pF0wZNmNTPpupXvLK4LU-_aENW5eJoXmOIcq6-yP_oAktOtgFrs5_bxGAs9xzoDATrddGfuRtOkihfEKqGWAK4=s1600-w12000",
         rating: 4.6,
         title: "Reserva Marina La Cueva del Indio",
         website:
@@ -114,7 +116,8 @@ trips.push(
           "Saturday: 9:30 AM - 5:00 PM",
           "Sunday: 9:30 AM - 5:00 PM",
         ],
-        photo: "https://maps.google.com/maps/contrib/112788636437282198896",
+        photo:
+          "https://lh3.googleusercontent.com/places/AJDFj40_rxMBjFmgXyB3HcS4IT3fCgfqKY7LP8tEW6u1ryrtY_csNTJyvYhic7buRuwVqkl97O_eQOr_D7a4l0abVZt1RlBQBhkevpI=s1600-w4128",
         rating: 4.5,
         title: "Puente de Piedra",
         website: "",
@@ -142,7 +145,8 @@ trips.push(
           "Saturday: 9:30 AM - 5:00 PM",
           "Sunday: 9:30 AM - 5:00 PM",
         ],
-        photo: "https://maps.google.com/maps/contrib/106338701735648905646",
+        photo:
+          "https://lh3.googleusercontent.com/places/AJDFj41RQeWOfs8Qw5_2BKrXkE1hu3mBdS8r0o_i4SvJeu7XRD5q8nYPxDeYfJuD9V0w3KHzNMRNe4ncqsra0McRk-9hWVaCy_OucS4=s1600-w880",
         rating: 4.7,
         title: "Campus Martius Park",
         website: "https://downtowndetroitparks.com/parks/campus-martius/",
@@ -159,7 +163,8 @@ trips.push(
           "Saturday: 2:00 PM - 12:00 AM",
           "Sunday: 12:00 - 6:00 PM",
         ],
-        photo: "https://maps.google.com/maps/contrib/1054…869014169318270",
+        photo:
+          "https://lh3.googleusercontent.com/places/AJDFj41c2FOTrGmKss0MhEwIqXvLYnmVR5Z-J6f3HmN6x5uGSNVTm8qqxokH2X6tx7UbSWxXkkupU12Vb-qG8cns84EH0heyzKlnz8A=s1600-w7639",
         rating: 4.6,
         title: "Fowling Warehouse Hamtramck",
         website: "https://fowlingwarehouse.com/hamtramck",
@@ -176,48 +181,13 @@ trips.push(
           "Saturday: 9:30 AM - 5:00 PM",
           "Sunday: 9:30 AM - 5:00 PM",
         ],
-        photo: "https://maps.google.com/maps/contrib/102378542986728774417",
+        photo:
+          "https://lh3.googleusercontent.com/places/AJDFj409FJE-tIzDOhemky7RTC42mZjjE_F0GfF9VwH8PfuD7c3OeYtghleYwFEnEESaRqj9ICoj9uJx7kl82bW_mnlO-kSDsJs9EEY=s1600-w4032",
         rating: 3.8,
         title: "University of Michigan Transportation Research Institute",
         website: "http://www.umtri.umich.edu/",
         address: "",
       },
-    ],
-  })
-);
-trips.push(
-  new Trip({
-    owner: users[8]._id,
-    members: [users[0], users[2], users[3], users[7], users[8]],
-    startDate: "2023-03-02",
-    endDate: "2023-03-09",
-    name: "Atlanta Salsa Bachata Festival",
-    locations: [
-      {
-        coordinates: { lat: 33.764, lng: -84.349 },
-        hours: [
-          "Monday: 9:30 AM - 5:00 PM",
-          "Tuesday: 9:30 AM - 5:00 PM",
-          "Wednesday: 9:30 AM - 5:00 PM",
-          "Thursday: 9:30 AM - 5:00 PM",
-          "Friday: 9:30 AM - 5:00 PM",
-          "Saturday: 9:30 AM - 5:00 PM",
-          "Sunday: 9:30 AM - 5:00 PM",
-        ],
-        photo: "https://maps.google.com/maps/contrib/117847082933568165218",
-        rating: 3.8,
-        title: "Little Five Points",
-        website: "",
-        address: "",
-      },
-      // {
-      //   title: "Ponce City Market",
-      //   coordinates: { lat: 33.774, lng: -84.366 },
-      // },
-      // {
-      //   title: "Piedmont Park",
-      //   coordinates: { lat: 33.787, lng: -84.376 },
-      // },
     ],
   })
 );
