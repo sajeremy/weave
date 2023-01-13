7edc4d9a6307c3da88b03109a72d493bdc603a43
Gleb Mirzayev <gmirzayev@gmail.com>
2023-01-12 21:43:44 -0500

    work on about page

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -24,12 +24,9 @@ const AboutPage = () => {
                 <i class="fa-brands fa-square-github"></i>
               </a>
             </div>
+            <p className="about-description">Flex Developer</p>
           </div>
-          <p className="about-description">
-            League of legends iron player. Misses most skillshots and best
-            suited for support roles. Has not made her bed in 25 years of
-            living. Mostly found in a state of panic.
-          </p>
+          
         </div>
         <div className="developer-container">
           <img
@@ -56,11 +53,7 @@ const AboutPage = () => {
               </a>
             </div>
           </div>
-          <p className="about-description">
-            Ex KGB Hacker known for swaying the 2020 US election. Grandmaster in
-            starcraft and has 250+ apms. Less than 110 WPM on monkeytype. Likes
-            boston cremes.
-          </p>
+          <p className="about-description">Frontend Lead</p>
         </div>
 
         <div className="developer-container">
@@ -69,35 +62,28 @@ const AboutPage = () => {
             src={require("../../assets/jeremy.png")}
             alt="Camera Shy"
           ></img>
-          <div>
-            <div className="developer-container-middle">
-              <div>
-                <h2 className="about-name"> Jeremy Santiago </h2>
-              </div>
-              <div className="social-container">
-                <a
-                  className="social-media-links"
-                  href="https://www.linkedin.com/in/jeremy-santiago-11b05367"
-                  target="_blank"
-                >
-                  <i class="fa-brands fa-linkedin"></i>
-                </a>
-                <a
-                  className="social-media-links"
-                  href="https://github.com/sajeremy"
-                  target="_blank"
-                >
-                  <i class="fa-brands fa-square-github"></i>
-                </a>
-              </div>
+          <div className="developer-container-middle">
+            <div>
+              <h2 className="about-name"> Jeremy Santiago </h2>
+            </div>
+            <div className="social-container">
+              <a
+                className="social-media-links"
+                href="https://www.linkedin.com/in/jeremy-santiago-11b05367"
+                target="_blank"
+              >
+                <i class="fa-brands fa-linkedin"></i>
+              </a>
+              <a
+                className="social-media-links"
+                href="https://github.com/sajeremy"
+                target="_blank"
+              >
+                <i class="fa-brands fa-square-github"></i>
+              </a>
             </div>
           </div>
-          <p className="about-description">
-            The hardest worker in this group... Jeremy made a living on the
-            poles prior to appacademy. Total earnings equate to $9.00 USD.
-            You'll find him playing hackysack or at Champion's pizza. This man
-            lives in the backend.
-          </p>
+          <p className="about-description">Backend Lead</p>
         </div>
 
         <div className="developer-container">
@@ -122,14 +108,10 @@ const AboutPage = () => {
                 target="_blank"
               >
                 <i class="fa-brands fa-square-github"></i>
-              </a>
+              </a>  
             </div>
           </div>
-          <p className="about-description">
-            Justin served as team lead and project flex. You can regularly hear
-            him drumming his Dorado belly after a good lunch. He handled the
-            implementation of API's on this project.
-          </p>
+          <p className="about-description">Team Lead - Flex Developer</p>
         </div>
       </div>
       <div>
